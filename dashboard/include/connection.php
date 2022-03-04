4683dc0418647cd194f790c11645663fa8fd21bf
Basheer1997 <basheeressam97@gmail.com>
2022-03-04 15:10:58 -0600

    data base name

--- dashboard/include/connection.php
+++ dashboard/include/connection.php
@@ -4,6 +4,6 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 //$user, $pass, 
-$dbName = 'pdfbooks';
+$dbName =     'pdfbooks';//this is database name
 
 $con = mysqli_connect ($host,$user, $pass,  $dbName);
